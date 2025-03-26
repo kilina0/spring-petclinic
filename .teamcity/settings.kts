9c479fb7fe28842bfff212d8cc33d0653a840d81
Ksenia.Ilina <ksenia.ilina@jetbrains.com>
2025-03-26 16:55:58 +0200

    fixed settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,11 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnMetric
+import jetbrains.buildServer.configs.kotlin.failureConditions.failOnMetricChange
+import jetbrains.buildServer.configs.kotlin.triggers.finishBuildTrigger
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 /*
@@ -26,17 +33,204 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2024.12"
 
 project {
+    description = "Spring PetClinic Application"
 
     vcsRoot(HttpsGithubComKilina0springPetclinicGitRefsHeadsMain)
 
     buildType(Build)
+    buildType(Test)
+    buildType(CodeQuality)
+    buildType(Deploy)
+
+    buildTypesOrder = arrayListOf(Build, Test, CodeQuality, Deploy)
 }
 
 object Build : BuildType({
     name = "Build"
+    description = "Build Spring PetClinic Application"
+
+    artifactRules = """
+        target/*.jar => build
+        target/classes => classes
+    """.trimIndent()
 
     vcs {
         root(HttpsGithubComKilina0springPetclinicGitRefsHeadsMain)
+    }
+
+    steps {
+        maven {
+            name = "Clean & Package"
+            goals = "clean package"
+            runnerArgs = "-Dmaven.test.failure.ignore=true -Dmaven.test.skip=true"
+            jdkHome = "%env.JDK_17%"
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = "+:*"
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+
+    requirements {
+        exists("env.JDK_17")
+    }
+})
+
+object Test : BuildType({
+    name = "Test"
+    description = "Run tests for Spring PetClinic Application"
+
+    artifactRules = """
+        target/surefire-reports => reports.zip
+        target/site/jacoco => jacoco-report.zip
+    """.trimIndent()
+
+    vcs {
+        root(HttpsGithubComKilina0springPetclinicGitRefsHeadsMain)
+    }
+
+    steps {
+        maven {
+            name = "Run Tests with JaCoCo"
+            goals = "clean test jacoco:report"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            jdkHome = "%env.JDK_17%"
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = "+:*"
+            enabled = false
+        }
+        finishBuildTrigger {
+            buildType = "${Build.id}"
+            successfulOnly = true
+        }
+    }
+
+    dependencies {
+        snapshot(Build) {
+            onDependencyFailure = FailureAction.FAIL_TO_START
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+
+    failureConditions {
+        failOnMetricChange {
+            metric = BuildFailureOnMetric.MetricType.TEST_COUNT
+            threshold = 10
+            units = BuildFailureOnMetric.MetricUnit.PERCENTS
+            comparison = BuildFailureOnMetric.MetricComparison.LESS
+            compareTo = build {
+                buildRule = lastSuccessful()
+            }
+        }
+    }
+
+    requirements {
+        exists("env.JDK_17")
+    }
+})
+
+object CodeQuality : BuildType({
+    name = "Code Quality"
+    description = "Run code quality checks"
+
+    artifactRules = """
+        target/checkstyle-result.xml => checkstyle.zip
+    """.trimIndent()
+
+    vcs {
+        root(HttpsGithubComKilina0springPetclinicGitRefsHeadsMain)
+    }
+
+    steps {
+        maven {
+            name = "Run Checkstyle"
+            goals = "checkstyle:checkstyle"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            jdkHome = "%env.JDK_17%"
+        }
+    }
+
+    triggers {
+        finishBuildTrigger {
+            buildType = "${Test.id}"
+            successfulOnly = true
+        }
+    }
+
+    dependencies {
+        snapshot(Test) {
+            onDependencyFailure = FailureAction.FAIL_TO_START
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+
+    requirements {
+        exists("env.JDK_17")
+    }
+})
+
+object Deploy : BuildType({
+    name = "Deploy"
+    description = "Deploy Spring PetClinic Application"
+
+    type = BuildTypeSettings.Type.DEPLOYMENT
+    enablePersonalBuilds = false
+    maxRunningBuilds = 1
+
+    vcs {
+        root(HttpsGithubComKilina0springPetclinicGitRefsHeadsMain)
+    }
+
+    steps {
+        script {
+            name = "Prepare Deployment"
+            scriptContent = """
+                echo "Preparing deployment environment..."
+                mkdir -p deploy
+            """.trimIndent()
+        }
+
+        script {
+            name = "Deploy Application"
+            scriptContent = """
+                echo "Deploying Spring PetClinic application..."
+                cp %teamcity.build.checkoutDir%/target/*.jar deploy/
+                echo "Application deployed successfully!"
+            """.trimIndent()
+        }
+    }
+
+    dependencies {
+        snapshot(CodeQuality) {
+            onDependencyFailure = FailureAction.FAIL_TO_START
+        }
+        artifacts(Build) {
+            buildRule = lastSuccessful()
+            artifactRules = "build/*.jar => target/"
+        }
+    }
+
+    requirements {
+        exists("env.JDK_17")
     }
 })
 
